eb80b1e4e06f3c165017500d88b2956394df477d
Elizabeth C. <awesomejunior2@gmail.com>
2024-08-25 12:18:02 -0400

    Added Auto Attack and Anti AFK

--- main.js
+++ main.js
@@ -6,8 +6,15 @@ const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
 const config = ini.parse(fs.readFileSync('./setup.ini', 'utf-8'));
 
-console.log(`CraftCMD by NaturalTwitch`)
+//Starting Logo
+console.log(`********************************`)
+console.log(`*          CraftCMD            *`)
+console.log(`*      By NaturalTwitch        *`)
+console.log(`********************************`)
+process.stdout.write(`\x1B]0;CraftCMD - Mursy Development\x07`);
 
+
+// Discord Bridge
 if (config.Advanced.discordBridge && config.Advanced.discord_token) {
     const client = new Discord.Client(
         {
@@ -27,7 +34,6 @@ if (config.Advanced.discordBridge && config.Advanced.discord_token) {
         }
     );
 
-
     const bot = mineflayer.createBot({
         host: config.General.serverIP,
         port: parseInt(config.General.serverPort),
@@ -41,23 +47,36 @@ if (config.Advanced.discordBridge && config.Advanced.discord_token) {
     bot.on('login', async () => {
         const channel = client.channels.cache.find((x) => (x.id === config.Advanced.channelID))
         console.log(`Signed in as ${bot.username}`);
-        await process.stdout.write(`\x1B]0;${bot.username}@${config.General.serverIP}\x07`);
         console.log(`Discord Bridge Enabled: Signed into ${client.user.username}`)
 
         const discordOnline = new Discord.EmbedBuilder()
             .setColor(`00FF00`)
-            .setTitle(`CraftCMD by NaturalTwitch`)
+            .setTitle(`CraftCMD - Mursy Development`)
+            .setThumbnail(`https://cdn.mursybot.com/sharing/CraftCMD.png`)
             .setDescription(`**${bot.username}** is Now Online! \n Server IP: ${config.General.serverIP}`)
 
 
         if (channel) {
             channel.send({ embeds: [discordOnline] })
         }
+        
     });
 
     bot.on('chat', (username, message, translate, jsonMsg, Matches) => {
 
     });
+
+    // bot.once('spawn', () => {
+    //     const physics = bot.physics;
+
+    //     physics.liquidAcceleration = 10
+    //     physics.waterInertia = 0.2
+    //     physics.negligeableVelocity = -1.0 // Default 0.003
+    //     // physics.gravity = 0.05 // Default 0.08
+    //     // physics.playerHeight = 1.0
+
+    //     console.log("Physics updated:", physics)
+    // })
 
     client.on('messageCreate', (message) => {
         const channel = config.Advanced.channelID
@@ -73,6 +92,10 @@ if (config.Advanced.discordBridge && config.Advanced.discord_token) {
 
     bot.on('messagestr', (message, messagePosition, jsonMsg, sender, verified) => {
         const channel = client.channels.cache.find((x) => (x.id === config.Advanced.channelID))
+
+        if(message.includes(`Next Shard`)) return;
+
+        if(message === "") return;
         if (channel) {
             channel.send(message)
         }
@@ -129,6 +152,48 @@ if (config.Advanced.discordBridge && config.Advanced.discord_token) {
         }
     })
 
+    if (config.AutoAttack.enable) {
+        setInterval(() => {
+            const hostileMobs = Object.values(bot.entities).filter(entity => entity.kind === 'Hostile mobs');
+
+            if (hostileMobs.length > 0) {
+                // Find the closest hostile mob
+                const closestMob = hostileMobs.reduce((closest, mob) => {
+                    const distance = bot.entity.position.distanceTo(mob.position);
+                    if (!closest || distance < closest.distance) {
+                        return { mob, distance };
+                    }
+                    return closest;
+                }, null).mob;
+
+                // Attack the closest hostile mob
+                bot.attack(closestMob, config.AutoAttack.armSwing);
+            }
+        }, 1000);
+    }
+
+    if (config.AntiAFK.antiAfk) {
+        setInterval(() => {
+            console.log(`[AntiAFK] Ran Command ${config.AntiAFK.afkCommand}`);
+            bot.chat(config.AntiAFK.afkCommand);
+        }, config.AntiAFK.afkTime);
+    }
+
+    bot.on('kicked', (r) => {
+        console.log(`You were kicked for ${r}`)
+        setTimeout(() => {
+            process.exit()
+        }, config.Advanced.rejoinTime)
+    });
+
+    bot.on('error', (e) => {
+        console.log(`Disconnected by error: ${e}`)
+        setTimeout(() => {
+            process.exit()
+        }, config.Advanced.rejoinTime)
+    })
+
+
     rl.on('line', async (msg) => {
         bot.chat(msg)
     })
@@ -149,7 +214,6 @@ if (config.Advanced.discordBridge && config.Advanced.discord_token) {
 
     bot.on('login', async () => {
         console.log(`Signed in as ${bot.username}`);
-        await process.stdout.write(`\x1B]0;${bot.username}@${config.General.serverIP}\x07`);
     });
 
     bot.on('chat', (username, message, translate, jsonMsg, Matches) => {
@@ -158,6 +222,9 @@ if (config.Advanced.discordBridge && config.Advanced.discord_token) {
 
     bot.on('messagestr', (message, messagePosition, jsonMsg, sender, verified) => {
         const tpRequest = /^(.*) has requested to teleport to you\.|^(.*) has requested that you teleport to them\.$/;
+
+        if(message.includes(`Next Shard`)) return;
+
         const match = message.match(tpRequest);
 
         if (match) {
@@ -177,8 +244,48 @@ if (config.Advanced.discordBridge && config.Advanced.discord_token) {
         // console.log(`${message}, ${messagePosition}, ${jsonMsg}, ${sender}, ${verified}`);
     });
 
+    bot.on('kicked', (r) => {
+        console.log(`You were kicked for ${r}`)
+        setTimeout(() => {
+            process.exit()
+        }, config.Advanced.rejoinTime)
+    });
+
+    bot.on('error', (e) => {
+        console.log(`Disconnected by error: ${e}`)
+        setTimeout(() => {
+            process.exit()
+        }, config.Advanced.rejoinTime)
+    })
+
     rl.on('line', async (msg) => {
         bot.chat(msg)
     })
+
+    if (config.AutoAttack.enable) {
+        setInterval(() => {
+            const hostileMobs = Object.values(bot.entities).filter(entity => entity.kind === 'Hostile mobs');
+
+            if (hostileMobs.length > 0) {
+                // Find the closest hostile mob
+                const closestMob = hostileMobs.reduce((closest, mob) => {
+                    const distance = bot.entity.position.distanceTo(mob.position);
+                    if (!closest || distance < closest.distance) {
+                        return { mob, distance };
+                    }
+                    return closest;
+                }, null).mob;
+
+                // Attack the closest hostile mob
+                bot.attack(closestMob);
+            }
+        }, 1000);
+    }
+
+    if (config.AntiAFK.antiAfk) {
+        setInterval(() => {
+            bot.chat(config.AntiAFK.afkCommand)
+        }, config.AntiAFK.afkTime)
+    }
 
 }
