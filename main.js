f4d66e7903662fdef64a212e55aa4a249f9dcc67
Elizabeth C. <awesomejunior2@gmail.com>
2024-09-20 08:56:24 -0400

    added github link to update message

--- main.js
+++ main.js
@@ -529,7 +529,8 @@ async function checkForUpdates() {
             try {
                 const remoteVersion = JSON.parse(data).version;
                 if (remoteVersion !== packageVersion) {
-                    console.log("\x1b[33m%s\x1b[0m", `[Update Available] A new version (${remoteVersion}) is available. \nCurrent version is (${packageVersion}). \nPlease update your bot.`);
+                    console.log("\x1b[33m%s\x1b[0m", `[Update Available] A new version (${remoteVersion}) is available. \nCurrent version is (${packageVersion}). \nPlease update your bot.
+                         \nhttps://github.com/NaturalTwitch/CraftCMD`);
                 } else {
                     console.log("\x1b[32m%s\x1b[0m", `[CraftCMD] Your version (${packageVersion}) is up to date.`);
                 }
